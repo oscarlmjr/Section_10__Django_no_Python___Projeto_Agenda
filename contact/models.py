05ad0f7b73ff79b0b8adf1f4505efe7a832170c0
oscarlmjr <oscarlmjr@gmail.com>
2026-02-17 11:18:03 -0300

    secao_10 aula_461

--- contact/models.py
+++ contact/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
-
 # id (primary key - automático)
 # first_name (string),  last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 # category (foreign key), show (boolean), picture (imagem)
 
-
 # Depois
 # owner (foreign key)
+
+
 class Category(models.Model):
+	class Meta:
+		verbose_name = 'Category'
+		verbose_name_plural = 'Categories'
+
 	name = models.CharField(max_length=50)
 
 	def __str__(self) -> str:
