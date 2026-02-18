d8eab08c417612a7f454592d5aaf96bfb4a5d4f0
oscarlmjr <oscarlmjr@gmail.com>
2026-02-18 15:14:59 -0300

    secao_10 aula_472

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,5 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-
 from contact.models import Contact
 
 
@@ -11,6 +10,7 @@ def index(request):
 	
 	context = {
 		'contacts': contacts,
+		'site_title': 'Contatos - '
 	}
 
 	return render(
@@ -25,12 +25,15 @@ def contact(request, contact_id):
 	single_contact = get_object_or_404(
 		Contact, pk=contact_id, show=True
 		)
+	site_title = f'{single_contact.first_name} \
+		{single_contact.last_name} - '
 
 	if single_contact is None:
 		raise Http404()
 
 	context = {
 		'contact': single_contact,
+		'site_title': site_title
 	}
 
 	return render(
