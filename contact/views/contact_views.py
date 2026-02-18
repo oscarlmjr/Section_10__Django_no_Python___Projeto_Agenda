66b34ffbea9ec770a9fcfe8b9c355df4a02ba37c
oscarlmjr <oscarlmjr@gmail.com>
2026-02-18 13:59:10 -0300

    secao_10 aula_470

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from contact.models import Contact
 
@@ -8,8 +9,6 @@ def index(request):
 		.filter(show=True)\
 		.order_by('-id')[10:20]
 	
-	print(contacts.query)
-
 	context = {
 		'contacts': contacts,
 	}
@@ -17,5 +16,25 @@ def index(request):
 	return render(
 		request,
 		'contact/index.html',
+		context
+	)
+
+
+def contact(request, contact_id):
+	# single_contact = Contact.objects.filter(pk=contact_id).first()
+	single_contact = get_object_or_404(
+		Contact, pk=contact_id, show=True
+		)
+
+	if single_contact is None:
+		raise Http404()
+
+	context = {
+		'contact': single_contact,
+	}
+
+	return render(
+		request,
+		'contact/contact.html',
 		context
 	)
