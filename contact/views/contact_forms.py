40c0ddf373338be25301cfbf92ce2f8fef853ac9
oscarlmjr <oscarlmjr@gmail.com>
2026-02-19 20:57:50 -0300

    secao_10 aula_481

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,24 +1,33 @@
+from django import forms
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
+class ContactForm(forms.ModelForm):
+	class Meta:
+		model = Contact
+		fields = (
+			'first_name', 'last_name', 'phone',
+			)
+	
 
 def create(request):
-	if request == 'POST':
-		print()
-		print(request.method)
-		print(request.POST.get('first_name'))
-		print(request.POST.get('last_name'))
-		print()
+	if request.method == 'POST':
+		context = {
+			'form': ContactForm(request.POST)
+		}
 
+		return render(
+			request, 
+			'contact/create.html',
+			context
+		)
+	
 	context = {
-		
+		'form': ContactForm()
 	}
-
-	print()
-	print(request.method)
 
 	return render(
 		request, 
