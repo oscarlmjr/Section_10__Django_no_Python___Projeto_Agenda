0444c8865bde38a652d5b3dba41d4d4a1912b274
oscarlmjr <oscarlmjr@gmail.com>
2026-02-19 14:56:46 -0300

    secao_10 aula_476

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-	path('<int:contact_id>/', views.contact, name='contact'),
-	path('search/', views.search, name='search'),
 	path('', views.index, name='index'),
+	path('search/', views.search, name='search'),
+
+	# contact (CRUD)
+	path('user/<int:contact_id>/detail/', views.contact, name='contact'),
+# 	path('user/create/', views.contact, name='contact'),
+# 	path('user/<int:contact_id>/update/', views.contact, name='contact'),
+# 	path('user/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
