8f18ee7df121b70484c580011249fe1adbc9333e
oscarlmjr <oscarlmjr@gmail.com>
2026-02-19 14:27:31 -0300

    secao_10 aula_475

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,20 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import Http404
 from contact.models import Contact
-
 
 def index(request):
 	contacts = Contact.objects \
 		.filter(show=True)\
-		.order_by('-id')[10:20]
+		.order_by('-id')
 	
+	paginator = Paginator(contacts, 10)
+	page_number = request.GET.get("page")
+	page_obj = paginator.get_page(page_number)
+
 	context = {
-		'contacts': contacts,
+		'page_obj': page_obj,
 		'site_title': 'Contatos - '
 	}
 
@@ -36,10 +40,14 @@ def search(request):
 			Q(phone__icontains=search_value) |
 			Q(email__icontains=search_value)
 			)\
-		.order_by('-id')
-	
+		.order_by('-id')	
+
+	paginator = Paginator(contacts, 10)
+	page_number = request.GET.get("page")
+	page_obj = paginator.get_page(page_number)
+
 	context = {
-		'contacts': contacts,
+		'page_obj': page_obj,
 		'site_title': 'Search - ',
 		'search_value': search_value,
 	}
