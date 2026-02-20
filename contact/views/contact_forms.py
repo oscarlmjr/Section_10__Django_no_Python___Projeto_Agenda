d71e83d1ba244df4ce59b4a84cc2f0f0881d0e99
oscarlmjr <oscarlmjr@gmail.com>
2026-02-20 11:17:50 -0300

    secao_10 aula_481_482

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,6 @@
+from typing import Any, Dict
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
@@ -11,6 +13,27 @@ class ContactForm(forms.ModelForm):
 		fields = (
 			'first_name', 'last_name', 'phone',
 			)
+		
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		
+		self.add_error(
+			'first_name',
+			ValidationError(
+				'Mensagem de erro',
+				code='invalid'
+			)
+		)
+		
+		self.add_error(
+			'first_name',
+			ValidationError(
+				'Mensagem de erro 2',
+				code='invalid'
+			)
+		)
+		
+		return super().clean()
 	
 
 def create(request):
