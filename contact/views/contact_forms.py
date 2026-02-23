60037780422722bcaafe79785f741aa453585669
oscarlmjr <oscarlmjr@gmail.com>
2026-02-22 22:24:37 -0300

    secao_10 aula_489

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -44,7 +44,6 @@ def update(request, contact_id):
 		)
 	form_action = reverse('contact:update', args=(contact_id,))
 
-
 	if request.method == 'POST':
 		form = ContactForm(request.POST, instance=contact)
 
@@ -72,4 +71,23 @@ def update(request, contact_id):
 		request, 
 		'contact/create.html',
 		context
+	)
+
+def delete(request, contact_id):
+	contact = get_object_or_404(
+		Contact, pk=contact_id, show=True
+		)
+	confirmation = request.POST.get('confirmation', 'no')
+
+	if confirmation == 'yes':
+		contact.delete()	
+		return redirect ('contact:index')
+
+	return render(
+		request,
+		'contact/contact.html',
+		{
+			'contact': contact,
+			'confirmation': confirmation,
+		}
 	)
